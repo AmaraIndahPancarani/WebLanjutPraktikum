f5ddc956fac82998821d96988b9c7688477f17ff
AmaraIndahPancarani <amaraindah24@gmail.com>
2020-10-23 17:14:38 +0700

    PrakWebLanjut

---
+++ app/Controllers/Helloworld.php
@@ -0,0 +1,17 @@
+<?php namespace App\Controllers;
+
+class Helloworld extends BaseController {
+
+    public function index($name, $npm)
+    {
+        // echo $this->name;
+        echo $name ."<br>";
+        echo $npm ."<br>";
+        // echo "Hello Amara";
+    }
+    public function show()
+    {
+        echo "Amara Indah";
+        echo "1817051010";
+    }
+}
